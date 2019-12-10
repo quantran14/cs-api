cdf1b380a20a848da12dec70a52e75044471688b
Quan <quantran.quou.6666@gmail.com>
2019-12-11 01:31:40 +0700

    api?

---
+++ mmlab_api/detectron_2_api/views.py
@@ -0,0 +1,25 @@
+import os
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+
+
+def upload_images(request):
+    print(request.FILES)
+    img = request.FILES.get('image')
+
+    file_saving = FileSystemStorage(settings.MEDIA_ROOT, settings.MEDIA_URL)
+    file_saving.save(img.name, img)
+
+
+class Image(APIView):
+
+    def post(self, request, *args, **kwargs):
+        upload_images(request=request)
+
+        return Response({'success': 'accepted'}, status= status.HTTP_202_ACCEPTED)
