04c34fd49d06b070bc5175a3f18d3a065909fc8a
Quan <quantran.quou.6666@gmail.com>
2019-12-26 20:48:49 +0700

    miss ' ' in config

--- mmlab_api/insightface_api/configs.py
+++ mmlab_api/insightface_api/configs.py
@@ -1,10 +1,12 @@
 import insightface
 
-arcface_r100_v1 = insightface.model_zoo.get_model(arcface_r100_v1)
-retinaface_r50_v1 = insightface.model_zoo.get_model(retinaface_r50_v1)
-retinaface_mnet025_v1 = insightface.model_zoo.get_model(retinaface_mnet025_v1)
-retinaface_mnet025_v2 = insightface.model_zoo.get_model(retinaface_mnet025_v2)
-genderage_v1 = insightface.model_zoo.get_model(genderage_v1)
+arcface_r100_v1 = insightface.model_zoo.get_model('arcface_r100_v1')
+retinaface_r50_v1 = insightface.model_zoo.get_model('retinaface_r50_v1')
+retinaface_mnet025_v1 = insightface.model_zoo.get_model(
+    'retinaface_mnet025_v1')
+retinaface_mnet025_v2 = insightface.model_zoo.get_model(
+    'retinaface_mnet025_v2')
+genderage_v1 = insightface.model_zoo.get_model('genderage_v1')
 
 _models = {
     'arcface_r100_v1': arcface_r100_v1,
