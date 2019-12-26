6e81bd94565746eb3217e72c9497373fdce78201
Quan <quantran.quou.6666@gmail.com>
2019-12-26 21:44:39 +0700

    🐞🐞🐞

--- mmlab_api/insightface_api/configs.py
+++ mmlab_api/insightface_api/configs.py
@@ -3,19 +3,28 @@ import insightface
 
 print('[INFO]Create model ...')
 arcface_r100_v1 = insightface.model_zoo.get_model('arcface_r100_v1')
+arcface_r100_v1.prepare(ctx_id=1, nms=0.7)
+
 retinaface_r50_v1 = insightface.model_zoo.get_model('retinaface_r50_v1')
+retinaface_r50_v1.prepare(ctx_id=1, nms=0.7)
+
 retinaface_mnet025_v1 = insightface.model_zoo.get_model(
     'retinaface_mnet025_v1')
+retinaface_mnet025_v1.prepare(ctx_id=1, nms=0.7)
+
 retinaface_mnet025_v2 = insightface.model_zoo.get_model(
     'retinaface_mnet025_v2')
-genderage_v1 = insightface.model_zoo.get_model('genderage_v1')
+retinaface_mnet025_v2.prepare(ctx_id=1, nms=0.7)
 
-_models = {
-    'arcface_r100_v1': arcface_r100_v1.prepare(ctx_id=1, nms=0.7),
-    'retinaface_r50_v1': retinaface_r50_v1.prepare(ctx_id=1, nms=0.7),
-    'retinaface_mnet025_v1': retinaface_mnet025_v1.prepare(ctx_id=1, nms=0.7),
-    'retinaface_mnet025_v2': retinaface_mnet025_v2.prepare(ctx_id=1, nms=0.7),
-    'genderage_v1': genderage_v1.prepare(ctx_id=1, nms=0.7),
+genderage_v1 = insightface.model_zoo.get_model('genderage_v1')
+genderage_v1.prepare(ctx_id=1, nms=0.7)
+
+models = {
+    'arcface_r100_v1': arcface_r100_v1,
+    'retinaface_r50_v1': retinaface_r50_v1,
+    'retinaface_mnet025_v1': retinaface_mnet025_v1,
+    'retinaface_mnet025_v2': retinaface_mnet025_v2,
+    'genderage_v1': genderage_v1,
 }
 
 
