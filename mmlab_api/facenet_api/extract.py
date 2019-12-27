82775c807e2343908e9bf2b4c3c998d42e6e5eea
Quan <quantran.quou.6666@gmail.com>
2019-12-27 14:49:16 +0700

    debug

--- mmlab_api/facenet_api/extract.py
+++ mmlab_api/facenet_api/extract.py
@@ -12,7 +12,9 @@ class FaceNetFeatureExtractor(object):
 
     def make_extraction(self, data):
         image = data['image']
+        print('image shape ', image.shape)
         image = np.expand_dims(image, axis=0)
+        print('image after shape ', image.shape)
         features = self.model.predict(image)
         data.update({'features': features})
 
